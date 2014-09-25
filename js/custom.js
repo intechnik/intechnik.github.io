d7f4f591a47f873a1eaa4de5729bd8c79477cc3e
Mikolaj Piotr Wysoczanski <mikolaj.wysoczanski@gmail.com>
2014-09-25 11:59:31 +0200

    Add custom dimention to GoogleAnalyticsObject [#10]

--- js/custom.js
+++ js/custom.js
@@ -2,27 +2,28 @@ var INTECHNIK_LATEST_TAB_KEY = 'intechnik.latestTab';
 var INTECHNIK_LATEST_TAB_CHANGE_KEY = 'intechnik.latestTabChange';
 var EXPIRE_TIME_IN_MILISECONDS = 43200000; //is equal to 0.5 day
 
+var INIT_BODY_PADDING = '70px';
+
 $(document).ready(function () {
 
-	goToLastTab();
+	$('#navbarBrand').click(function () {
+		$('a[href="#ofirmie"]').click();
+	});
 	
-	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+	$('a[data-toggle="tab"]').click(function (e) {
 		saveLastTab(e.target.hash);
 		hideMenuIfNeeded();
 	})
 	
-	$('#navbarBrand').click(function () {
-		$('a[href="#ofirmie"]').tab('show');
-		hideMenuIfNeeded();
-	});
+	googleAnalytics();
+
+	goToLastTab();
 	
-	var initBodyPadding = '70px';
-	
-	$("body").css("padding-top", initBodyPadding).css("padding-bottom", initBodyPadding);
+	$("body").css("padding-top", INIT_BODY_PADDING).css("padding-bottom", INIT_BODY_PADDING);
 	
 	$('#mainNavbar').on('shown.bs.collapse', function () { $("body").css("padding-top", "+=" + $(this).height()); });
 	
-	$('#mainNavbar').on('hidden.bs.collapse', function () { $("body").css("padding-top", initBodyPadding); });
+	$('#mainNavbar').on('hidden.bs.collapse', function () { $("body").css("padding-top", INIT_BODY_PADDING); });
 });
 
 function hideMenuIfNeeded() {
@@ -53,4 +54,14 @@ function isLessThanExpirationTime(periodFromLastChange) {
 
 function getCurrentTime() {
 	return new Date().getTime().toString();
+}
+
+function googleAnalytics() {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-2442307-3', 'auto');
+	ga('send', 'pageview');
+	ga('set', 'dimension1', localStorage.getItem(INTECHNIK_LATEST_TAB_KEY));
 }
